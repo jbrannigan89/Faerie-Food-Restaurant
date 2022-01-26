5d2adf256fdef75b2d7dbaff6999b01c1fb43dec
Joe Brannigan <jbrannigan89@gmail.com>
2022-01-26 17:27:10 -0500

    bunch of changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,15 +110,13 @@ const sr = ScrollReveal({
 });
 
 sr.reveal(
-  `.home__data, .home__img,
+  `.home__data,
             .about__data, .about__img,
-            .services__content, .menu__content,
+            .services__content, .menu__content,.contact-form,
             .app__data, .app__img,
-            .contact__data, .contact__button,
-            .footer__content`,
+            .contact__data, .contact__button,.gallery-title
+            .footer__content, .swiper`,
   {
     interval: 200,
   }
 );
-
-
